cd0d91cb3924fbdf6593615ab90571db700d6f58
MohammadRehanPatel <88241322+MohammadRehanPatel@users.noreply.github.com>
2025-07-14 11:18:43 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,4 @@
 def call(String ProjectName, String ImageTag, String DockerHubUser){
+        echo "This is Building the code"
   bat "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
